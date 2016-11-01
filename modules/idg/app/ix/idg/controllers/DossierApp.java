16ced6a6256862896766011002065b62925c2953
Rajarshi Guha <rajarshi.guha@gmail.com>
2016-11-01 17:33:54 -0400

    Added backend methods to intersect folders

--- modules/idg/app/ix/idg/controllers/DossierApp.java
+++ modules/idg/app/ix/idg/controllers/DossierApp.java
@@ -221,7 +221,43 @@ public class DossierApp extends App implements Commons {
         return ok(String.valueOf(_countEntities()));
     }
 
+    public static Result intersectFolders(String folder1, String folder2, String newFolderName) throws IOException {
+        if (newFolderName == null || newFolderName.trim().equals(""))
+            return _badRequest("Must provide a valid folder name for merged folder");
+
+        ArrayNode cart = getCartFromSession();
+        ObjectNode nfolder = mapper.createObjectNode();
+        nfolder.put("folder", newFolderName);
+        nfolder.put("entities", mapper.createArrayNode());
+        ArrayNode entities = (ArrayNode) nfolder.get("entities");
+
+        ObjectNode f1 = getFolderFromCart(cart, folder1);
+        ObjectNode f2 = getFolderFromCart(cart, folder2);
+
+        ArrayNode e1 = (ArrayNode) f1.get("entities");
+        ArrayNode e2 = (ArrayNode) f2.get("entities");
+
+        for (int i = 0; i < e1.size(); i++) {
+            ObjectNode node = (ObjectNode) e1.get(i);
+            if (cartContains(cart, folder2, node.get("type").textValue(), node.get("entity").textValue()))
+                entities.add(node);
+        }
+        for (int i = 0; i < e2.size(); i++) {
+            ObjectNode node = (ObjectNode) e2.get(i);
+            if (cartContains(cart, folder1, node.get("type").textValue(), node.get("entity").textValue()))
+                entities.add(node);
+        }
+        cart.add(nfolder);
+        session().put("cart", mapper.writeValueAsString(cart));
+        ObjectNode ret = mapper.createObjectNode();
+        ret.put("total_entries", _countEntities());
+        ret.put("total_collections", _countFolders());
+        return ok(ret);
+    }
+
     public static Result mergeFolders(String folderNames, String newFolderName) throws IOException {
+        if (folderNames == null)
+            return _badRequest("Must specify 2 or more folders to merge");
         String[] folderNameArray = folderNames.split(",");
         if (folderNameArray.length < 2)
             return _badRequest("Must specify 2 or more folders to merge");
